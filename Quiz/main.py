f9f0199cc0305b5928b874ec41ffde1981871f22
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2026-01-18 21:19:04 -0500

    Implement quiz functionality in main.py

---
+++ Quiz/main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+
+question_bank = []
+for i in question_data:
+    question_bank.append(Question(i['text'],i['answer']))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz.\nYour final score was: {quiz.score}/{quiz.question_number}")
