9164ed13e08db0401ceb0f52afb2b9dc11005350
Samadar Oripov <144627379+smdr111@users.noreply.github.com>
2026-05-18 15:16:03 -0400

    Implement email sending for birthday reminders
    Added email sending functionality for birthday wishes.

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -12,14 +12,13 @@ day = now.day
 month = now.month
 data = pandas.read_csv('birthdays.csv').to_dict(orient='records')
 
-# Check if today matches a birthday in the birthdays.csv
 for birth in data:
     if birth['month']==month and birth['day']==day:
         file_path = f"letter_templates/letter_{rd.randint(1,3)}.txt"
         with open(file_path,'r') as f:
             content = f.read()
         content=content.replace('[NAME]',birth['name'])
-        #Send the letter generated in step 3 to that person's email address.
+        #-------SEND EMAIL-------#
         with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
             connection.starttls()
             connection.login(user=my_email, password=password)
